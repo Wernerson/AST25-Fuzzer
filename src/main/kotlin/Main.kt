864529ae4564006f81e0b1dcaa6ccd6378eaacc4
Sebastian Brunner <brunner.sebi4@gmail.com>
2025-05-11 23:18:40 +0200

    add version param

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,20 +1,26 @@
 package net.sebyte
 
+import kotlinx.cli.ArgParser
+import kotlinx.cli.ArgType
 import kotlinx.cli.ExperimentalCli
+import kotlinx.cli.default
 import net.sebyte.cfg.SQLITE_v3_26_0
+import net.sebyte.cli.Logger
 import net.sebyte.run.*
 import java.io.File
 
 
 @OptIn(ExperimentalCli::class)
 fun main(args: Array<String>) {
-//    val parser = ArgParser("test-db")
-//    val compare = CompareTask()
-//    val crash = CrashTask()
-//    val queries = QueriesTask()
-//    val coverage = CoverageTask()
-//    parser.subcommands(compare, crash, queries, coverage)
-//    parser.parse(args)
+    val parser = ArgParser("test-db")
+
+    val version by parser.option(ArgType.Boolean, "version", "v", "Display version").default(false)
+    parser.parse(args)
+
+    if (version) {
+        Logger.info { "v0.1.0 by Sebastian Brunner" }
+        return
+    }
 
     val preparator = TestDbPreparator(SQLITE_v3_26_0, 20, 5, "sqlite3-3.26.0", File("."))
 //    val preparator = Preparator(SQLITE_v3_26_0, 20, 5)
